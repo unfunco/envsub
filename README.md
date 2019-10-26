6a14408622eb791145b5bc2b2081531dec0834fb
Daniel Morris <daniel@honestempire.com>
2019-10-26 12:35:03 +0100

    Add initial test workflow

--- README.md
+++ README.md
@@ -3,33 +3,11 @@
 ## Getting started
 
 ```yaml
-apiVersion: v1
-kind: Deployment
-metadata:
-  labels:
-    app: example
-  name: example-deployment
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: example
-  template:
-    metadata:
-      labels:
-        app: example
-    spec:
-      containers:
-      - name: example
-        image: ${IMAGE}
-        ports:
-        - containerPort: ${PORT}
-```
-
-```bash
-export IMAGE="nginx:latest"
-PORT="80" envsub < deployment.yaml > /tmp/deployment.yaml
-cat /tmp/deployment.yaml
+containers:
+- name: example-container
+  image: ${IMAGE}
+  ports:
+  - containerPort: ${PORT}
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-/*
- * Copyright © 2019 Daniel Morris
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//! Crate `envsub` provides environment substitution.
+
+// Copyright © 2019 Daniel Morris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use std::io::{self, BufRead};
 
@@ -23,13 +23,19 @@ fn main() {
     }
 }
 
+/// Expands a string.
+pub fn expand(input: &str) -> String {
+    let output = String::from(input);
+    output
+}
+
 #[cfg(test)]
 mod tests {
     #[allow(unused_imports)]
     use super::*;
 
     #[test]
-    fn test_example() {
-        assert_eq!(1, 1)
+    fn test_expand() {
+        assert_eq!("Hello, world!", expand("Hello, world!"));
     }
 }
